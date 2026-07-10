03160c75ae002a3bd533228d8c45385a660f5dd5
Marko G <m@gaj.st>
2026-07-11 01:13:25 +0200

    chore: scaffold keymap service

--- src/features/app/index.ts
+++ src/features/app/index.ts
@@ -2,6 +2,7 @@ export * as app from "./app.service"
 export * as bus from "./bus.service"
 export * as log from "./log.service"
 export * as settings from "./settings.service"
+export * as keymap from "./keymap.service"
 
 export * as cfg from "./lib/cfg"
 export * as fs from "./lib/fs"
